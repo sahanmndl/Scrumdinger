86a3ded35e8c86184941825eacb80622952a216f
sahanmndl <mondalsahan053@gmail.com>
2022-07-31 13:27:22 +0530

    Backlog View

--- src/views/drawer/BacklogView.js
+++ src/views/drawer/BacklogView.js
@@ -1,12 +1,82 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, StyleSheet, View, Alert, Dimensions, Platform } from "react-native";
+import { useIsFocused } from "@react-navigation/native";
+import Colors from '../../constants/Colors';
+import ProjectItem from "../../components/ProjectItem";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import NoResults from "../../components/NoResults";
 
 const BacklogView = () => {
+
+    const isFocused = useIsFocused()
+    const [projects, setProjects] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [refresh, setRefresh] = useState(false)
+
+    const readProjects = async () => {
+        const userId = await AsyncStorage.getItem('userId')
+        console.log(userId)
+        try {
+            setLoading(true)
+            await axios.get(`http://10.2.71.238:8000/api/project/user/${userId}`)
+                .then((response) => {
+                    var json = response.data.projects.projects
+                    var filteredJSON = json.filter(it => it.category == "backlogs")
+                    console.log(json, filteredJSON)
+                    setProjects([...filteredJSON])
+                })
+            return true
+        } catch (err) {
+            Alert.alert('Error!', err.message)
+            console.log(err)
+            return false
+        } finally {
+            setRefresh(false)
+            setLoading(false)
+        }
+    }
+
+    const onRefresh = () => {
+        setRefresh(true)
+        readProjects()
+    }
+
+    useEffect(() => {
+        readProjects()
+    }, [])
+
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>BacklogView</Text>
+        <View style={styles.container}>
+            <View style={{flex: 1}}>
+                {loading ? <ActivityIndicator size={'large'} color={Colors.BLUE} /> :
+                    <FlatList
+                        style={{flex: 1}}
+                        data={projects}
+                        numColumns={WIDTH < 768 ? 1 : 2}
+                        onRefresh={onRefresh}
+                        refreshing={refresh}
+                        ListEmptyComponent={NoResults}
+                        showsVerticalScrollIndicator={Platform.OS === 'web' ? true : false}
+                        keyExtractor={({_id}) => _id}
+                        renderItem={({item}) => (
+                            <ProjectItem item={item} />
+                        )}
+                    />
+                }
+            </View>
         </View>
     )
 }
 
 export default BacklogView
+
+const WIDTH = Dimensions.get('window').width
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
+        backgroundColor: Colors.WHITISH2
+    }
+})
